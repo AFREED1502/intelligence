63ef6ea9b7bd6adb1a235c160f813f64dcd06427
AFREED1502 <sayyadafreed.work@gmail.com>
2024-10-09 10:11:12 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,36 @@
+// first code in js
+// variables
+// a=13;
+// b=15;
+// console.log(b);
+// c=23;
+
+// var a=10;
+// var b="Javascript";
+// console.log(a);
+// console.log(b);
+// console.log(a+b);
+
+
+// var c=true;
+// console.log(c+b+a);
+
+
+// var a="10";
+// let b='123';
+//  const c="123.456";
+//  console.log(a+b+c);
+
+
+var car="tata";
+let price="200002";
+
+price++;
+price++;
+price++;
+price++;
+
+console.log(price);
+
+
+
